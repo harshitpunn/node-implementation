c2bfe830812ce1481d45067073fad0c2a353ce01
Harshit Pun <harshitpunn@gmail.com>
2023-05-05 20:47:23 -0700

    ADDED challenges

---
+++ w1/justforfun/index.js
@@ -0,0 +1,10 @@
+let b;
+console.log(b);
+b = 5;
+console.log(b);
+
+// Hoisting
+
+// API vs SOAP, Event Loop, 3rd Part API integrations
+
+// HOC - Higher Order Components in React (very important)
