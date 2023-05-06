24d5c9a791ae1fdb4572da6079a94f50a1b15cc6
Harshit Pun <harshitpunn@gmail.com>
2023-05-05 18:04:21 -0700

    ADDED middlewares for cookies

--- w1/d5/index.js
+++ w1/d5/index.js
@@ -3,6 +3,15 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cookieSession({
+    name: 'user',
+    keys: ['key1', 'Key2'],
+  })
+);
+
 app.set('view engine', 'ejs');
 
 app.listen(8080, () => console.log('server running'));
