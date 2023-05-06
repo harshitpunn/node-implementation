a855b844ce1f274e264144b38f021720b7974c76
Harshit Pun <harshitpunn@gmail.com>
2023-05-05 18:46:47 -0700

    ADDED code for hashing the password

--- w1/d5/controllers/authController.js
+++ w1/d5/controllers/authController.js
@@ -1,7 +1,20 @@
+const { hashPassword } = require('../helpers/userHelper');
+
 const showRegisterForm = (req, res) => {
   res.render('register');
 };
 
+const registerUser = async (req, res) => {
+  const userName = req.body.username;
+  const hashedPassword = await hashPassword(req.body.password);
+
+  console.log(hashedPassword);
+
+  //req.session.username = userName;
+  // res.redirect('/user/profile');
+};
+
 module.exports = {
   showRegisterForm,
+  registerUser,
 };
--- w1/d5/routes/auth.js
+++ w1/d5/routes/auth.js
@@ -1,13 +1,16 @@
 const express = require('express');
 // We are going to import controller here so that we can access auth functions
 
-const { showRegisterForm } = require('../controllers/authController');
+const {
+  showRegisterForm,
+  registerUser,
+} = require('../controllers/authController');
 
 const router = express.Router();
 
 router.get('/register', showRegisterForm);
 
-//post on registration
+router.post('/register', registerUser);
 
 //get on login
 
