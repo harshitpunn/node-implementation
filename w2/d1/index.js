d69647790cdc6185cc3195b05c2124d57db93cd2
Harshit Pun <harshitpunn@gmail.com>
2023-05-16 18:15:58 -0700

    Hashing the password on day6

---
+++ w2/d1/index.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcrypt');
+
+const user = {
+  username: 'admin',
+  password: 'password',
+};
+
+const saltRounds = 12;
+
+const hashPassword = async (password, saltrounds) => {
+  console.time('hashing this password');
+  const salt = await bcrypt.genSalt(saltRounds);
+  console.log('salt', salt);
+
+  const hash = await bcrypt.hash(password, salt);
+  console.log('hash', hash);
+  console.timeEnd('hashing this password');
+
+  return hash;
+};
+
+hashPassword(user.password, saltRounds);
