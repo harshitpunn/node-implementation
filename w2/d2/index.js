0619bc60cca4efb41397a797d809ed9478cdf070
Harshit Pun <harshitpunn@gmail.com>
2023-05-17 19:17:12 -0700

    ADDED posts route

--- w2/d2/index.js
+++ w2/d2/index.js
@@ -1,7 +1,36 @@
+require('dotenv').config();
+
 const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 const app = express();
 const jwt = require('jsonwebtoken');
 
 app.use(express.json());
+
+const postData = [
+  {
+    title: 'ABC',
+    username: 'Harshit',
+  },
+  {
+    title: 'Magazine',
+    username: 'Clara',
+  },
+];
+
+app.post('/posts', authenticateToken, (req, res) => {});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['Authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(3000);
