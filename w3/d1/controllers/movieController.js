2e486c13a756f178d7b0c4a0b0053d1b248cd3f6
Harshit Pun <harshitpunn@gmail.com>
2023-05-23 18:25:09 -0700

    ADDED Authorization token

--- w3/d1/controllers/movieController.js
+++ w3/d1/controllers/movieController.js
@@ -7,7 +7,7 @@ const getMovieList = async (req, res) => {
       {
         headers: {
           accept: 'application/json',
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTg3MjdkYmI0M2ZkYWU5MGE4ZWNlODY1YmIxMTk2NyIsInN1YiI6IjYyNTFmZTNhOTA0ZjZkMDA1MjAzZmM1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0QRH7U28s6yfAKudM8hJwK6CwX9AmVnln4n3asoARR0`,
+          Authorization: `Bearer ${process.env.AUTHORIZATION_TOKEN}`,
         },
       }
     );
