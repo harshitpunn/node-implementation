a754cbb3a35e64de5d480ed89371cf0624a0f03b
Harshit Pun <harshitpunn@gmail.com>
2023-05-26 19:06:42 -0700

    ADDED fix

--- w3/d1/controllers/authenticationController.js
+++ w3/d1/controllers/authenticationController.js
@@ -34,7 +34,7 @@ const loginUser = async (req, res) => {
     if (await bcrypt.compare(password, user.password)) {
       const accessToken = generateAccessToken(user);
       res.json({ accessToken: accessToken });
-      res.end();
+      return;
     }
     res.json({
       error: true,
